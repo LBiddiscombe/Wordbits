32e10a1895698029ba5215df27d96d044e897068
LBiddiscombe <lee.biddiscombe@btinternet.com>
2019-02-24 13:57:30 +0000

    migrate to hooks

--- src/components/WordList/index.js
+++ src/components/WordList/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import './WordList.css'
 import Definition from '../Definition'
 
@@ -11,50 +11,53 @@ const LengthTitle = props => (
 )
 const Word = props => <li className="wordList__item">{props.word.toLowerCase()}</li>
 
-export default class WordList extends Component {
-  state = { selectedWord: '' }
+function WordList(props) {
+  const [selectedWord, setSelectedWord] = useState('')
 
-  onClick = e => {
+  useEffect(
+    () => {
+      setSelectedWord('')
+    },
+    [props]
+  )
+
+  const onClick = e => {
     e.preventDefault()
     if (e.target.tagName === 'SPAN') return
-    if (!this.state.selectedWord) {
-      this.setState({ selectedWord: e.target.textContent })
+    if (!selectedWord) {
+      setSelectedWord(e.target.textContent)
     } else {
-      this.setState({ selectedWord: '' })
+      setSelectedWord('')
     }
   }
 
-  componentWillReceiveProps(nextProps) {
-    this.setState({ selectedWord: '' })
-  }
+  const { words = [] } = props
 
-  render() {
-    const { words = [] } = this.props
+  let rows = []
+  let lastLength = 0
+  words.forEach(word => {
+    if (word.length !== lastLength) {
+      rows.push(<LengthTitle len={word.length} key={word.length} />)
+    }
+    rows.push(<Word word={word} key={word} />)
+    lastLength = word.length
+  })
 
-    let rows = []
-    let lastLength = 0
-    words.forEach(word => {
-      if (word.length !== lastLength) {
-        rows.push(<LengthTitle len={word.length} key={word.length} />)
-      }
-      rows.push(<Word word={word} key={word} />)
-      lastLength = word.length
-    })
+  const columns = Math.trunc(words.length / 15) + 1
 
-    const columns = Math.trunc(words.length / 15) + 1
-
-    return this.state.selectedWord ? (
-      <div onClick={this.onClick}>
-        <Definition word={this.state.selectedWord} />
-      </div>
-    ) : (
-      <ul
-        className="wordList"
-        style={{ '--column-count': columns, '--width': columns * 10 + 'rem' }}
-        onClick={this.onClick}
-      >
-        {rows}
-      </ul>
-    )
-  }
+  return selectedWord ? (
+    <div onClick={onClick}>
+      <Definition word={selectedWord} />
+    </div>
+  ) : (
+    <ul
+      className="wordList"
+      style={{ '--column-count': columns, '--width': columns * 10 + 'rem' }}
+      onClick={onClick}
+    >
+      {rows}
+    </ul>
+  )
 }
+
+export default WordList
