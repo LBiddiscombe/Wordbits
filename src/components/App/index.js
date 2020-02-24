382f0994a49bed9dfc8f18283fea0bf4626b8235
Lee Biddiscombe (Co-op IT - Retail) <lee.biddiscombe@coop.co.uk>
2020-02-24 21:07:07 +0000

    rename variables

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -12,65 +12,62 @@ const MAX_WILDCARDS = 7
 
 function App() {
   const [dictionary, setDictionary] = useState(null)
-  const [letters, setLetters] = useState('')
+  const [inputString, setInputString] = useState('')
   const [error, setError] = useState('')
 
   useEffect(() => {
     loadDictionary().then(dictionary => setDictionary(dictionary))
   }, [])
 
-  const onTextInputSubmit = letters => {
-    const wildcardCount = (letters.match(/\./g) || []).length
+  const onTextInputSubmit = value => {
+    const wildcardCount = (value.match(/\./g) || []).length
     if (wildcardCount > MAX_WILDCARDS) {
-      const error = `Max ${MAX_WILDCARDS} wildcards allowed`
-      setError(error)
+      setError(`Max ${MAX_WILDCARDS} wildcards allowed`)
       return
     }
 
-    const beginsWithCount = (letters.match(/\*/g) || []).length
+    const beginsWithCount = (value.match(/\*/g) || []).length
     if (beginsWithCount > 1) {
-      const error = `Only 1 begins with (${AS_WORD_START_CHAR}) wildcard allowed`
-      setError(error)
+      setError(`Only 1 begins with (${AS_WORD_START_CHAR}) wildcard allowed`)
       return
     }
 
-    if (beginsWithCount === 1 && letters.length - wildcardCount - 1 <= 1) {
-      const error = `At least 2 letters required for prefix search`
-      setError(error)
+    if (beginsWithCount === 1 && value.length - wildcardCount - 1 <= 1) {
+      setError(`At least 2 letters required for prefix search`)
       return
     }
 
-    setLetters(letters)
+    setInputString(value)
     setError('')
   }
 
   let words = undefined
   let duration = 0
   const wildcardFound =
-    letters.indexOf(WILDCARD_CHAR) !== -1 || letters.indexOf(AS_WORD_START_CHAR) !== -1
-  const useAllLetters = letters.slice(-1) === USE_ALL_CHAR
-  const asWordStart = letters.slice(-1) === AS_WORD_START_CHAR
+    inputString.indexOf(WILDCARD_CHAR) !== -1 || inputString.indexOf(AS_WORD_START_CHAR) !== -1
+  const useAllLetters = inputString.slice(-1) === USE_ALL_CHAR
+  const asWordStart = inputString.slice(-1) === AS_WORD_START_CHAR
 
-  if (!error && dictionary && letters.length > 0) {
+  if (!error && dictionary && inputString.length > 0) {
     const start = performance.now()
 
     if (wildcardFound) {
       //TODO: fails when something like "l.st*" is passed
       if (asWordStart) {
-        words = dictionary.getWordsBeginning(letters, WILDCARD_CHAR)
-      } else words = dictionary.getWordMatches(letters, WILDCARD_CHAR)
+        words = dictionary.getWordsBeginning(inputString, WILDCARD_CHAR)
+      } else words = dictionary.getWordMatches(inputString, WILDCARD_CHAR)
     } else {
-      words = dictionary.getAnagrams(letters, useAllLetters ? letters.length - 1 : 3)
+      words = dictionary.getAnagrams(inputString, useAllLetters ? inputString.length - 1 : 3)
     }
 
     duration = Math.round(performance.now() - start)
   }
 
-  let results = ''
+  let resultText = ''
   if (!error) {
-    results = words ? 'Found ' + words.length + ' results in ' + duration + 'ms' : ''
+    resultText = words ? 'Found ' + words.length + ' results in ' + duration + 'ms' : ''
     if (useAllLetters) {
-      results = results + ' using all letters'
+      resultText = resultText + ' using all letters'
     }
   }
 
@@ -86,7 +83,7 @@ function App() {
         </p>
         <TextInput handleSubmit={onTextInputSubmit} error={error} />
       </div>
-      <p className="app__results">{results}</p>
+      <p className="app__resulttext">{resultText}</p>
       <WordList words={words} duration={duration} />
     </div>
   )
