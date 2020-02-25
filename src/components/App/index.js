a54877c49a1f48d9b47e57f30fec0afe9c125c89
Lee Biddiscombe (Co-op IT - Retail) <lee.biddiscombe@coop.co.uk>
2020-02-25 17:42:07 +0000

    move search execution logic to dictionary

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,13 +3,8 @@ import './App.css'
 import './nprogress.css'
 import WordList from '../WordList'
 import TextInput from '../TextInput'
-import { validateSearch } from '../../modules/Dictionary'
+import { validateSearchString, executeSearch } from '../../modules/Dictionary'
 import { loadDictionary } from '../../modules/api'
-
-const WILDCARD_CHAR = '.'
-const USE_ALL_CHAR = '/'
-const AS_WORD_START_CHAR = '*'
-const MAX_WILDCARDS = 7
 
 function App() {
   const [dictionary, setDictionary] = useState(null)
@@ -24,43 +19,16 @@ function App() {
   }, [])
 
   const onTextInputSubmit = value => {
-    const error = validateSearch(value)
+    const error = validateSearchString(value)
     setError(error)
     if (!error) setInputString(value)
   }
 
   // when inputString changes get the results from the dictionary
   useEffect(() => {
-    let duration = 0
-    let results = undefined
-    const wildcardFound = inputString.indexOf(WILDCARD_CHAR) !== -1
-    const useAllLetters = inputString.slice(-1) === USE_ALL_CHAR
-    const asWordStart = inputString.slice(-1) === AS_WORD_START_CHAR
-
-    if (!error && dictionary && inputString.length > 0) {
-      const start = performance.now()
-      if (asWordStart) {
-        results = dictionary.getWordsBeginning(inputString, WILDCARD_CHAR)
-        setWords(results)
-      } else if (wildcardFound) {
-        results = dictionary.getWordMatches(inputString, WILDCARD_CHAR)
-        setWords(results)
-      } else {
-        results = dictionary.getAnagrams(inputString, useAllLetters ? inputString.length - 1 : 3)
-        setWords(results)
-      }
-      duration = Math.round(performance.now() - start)
-    } else {
-      setWords(undefined)
-    }
-
-    if (!error) {
-      let resultText = results ? 'Found ' + results.length + ' results in ' + duration + 'ms' : ''
-      setResultText(resultText)
-      if (useAllLetters) {
-        setResultText(resultText + ' using all letters')
-      }
-    }
+    const [words, resultText] = executeSearch(dictionary, inputString)
+    setWords(words)
+    setResultText(resultText)
   }, [inputString])
 
   return (
@@ -68,10 +36,7 @@ function App() {
       <div className="app__top">
         <h1 className="app__header">Wordbits</h1>
         <p className="app__hint">
-          Try <span className="app__hint--bold">listen</span>,{' '}
-          <span className="app__hint--bold">listen{AS_WORD_START_CHAR}</span>,{' '}
-          <span className="app__hint--bold">listen{USE_ALL_CHAR}</span> or{' '}
-          <span className="app__hint--bold">ha{WILDCARD_CHAR}e</span>
+          Try <b>listen</b>, <b>listen*</b>, <b>listen/</b> or <b>ha.e</b>
         </p>
         <TextInput handleSubmit={onTextInputSubmit} error={error} />
       </div>
--- src/modules/Dictionary/index.js
+++ src/modules/Dictionary/index.js
@@ -5,7 +5,7 @@ const USE_ALL_CHAR = '/'
 const AS_WORD_START_CHAR = '*'
 const MAX_WILDCARDS = 7
 
-const validateSearch = value => {
+const validateSearchString = value => {
   const wildcardCount = (value.match(/\./g) || []).length
   if (wildcardCount > MAX_WILDCARDS) {
     return `Max ${MAX_WILDCARDS} wildcards allowed`
@@ -23,4 +23,32 @@ const validateSearch = value => {
   return ''
 }
 
-export { Trie, validateSearch }
+const executeSearch = (dictionary, inputString) => {
+  let duration = 0
+  let results = undefined
+  let resultText = ''
+  const wildcardFound = inputString.indexOf(WILDCARD_CHAR) !== -1
+  const useAllLetters = inputString.slice(-1) === USE_ALL_CHAR
+  const asWordStart = inputString.slice(-1) === AS_WORD_START_CHAR
+
+  if (dictionary && inputString.length > 0) {
+    const start = performance.now()
+    if (asWordStart) {
+      results = dictionary.getWordsBeginning(inputString, WILDCARD_CHAR)
+    } else if (wildcardFound) {
+      results = dictionary.getWordMatches(inputString, WILDCARD_CHAR)
+    } else {
+      results = dictionary.getAnagrams(inputString, useAllLetters ? inputString.length - 1 : 3)
+    }
+    duration = Math.round(performance.now() - start)
+  }
+
+  if (results)
+    resultText = `Found  ${results.length} results in ${duration}ms ${
+      useAllLetters ? ' using all letters' : ''
+    }`
+
+  return [results, resultText]
+}
+
+export { Trie, validateSearchString, executeSearch }
